12168d89e6abbec9cf9b14b3a3e8ae1e815cedd6
Eric Jiang <64503660+Bruh-moment-555@users.noreply.github.com>
2022-10-03 21:53:20 -0400

    First Commit

--- src/helloMain.java
+++ src/helloMain.java
@@ -1,8 +1,8 @@
-import java.util*;
+import java.util.*;
 public class helloMain {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-        System.out.println("Hello from Person B!")
+        System.out.println("Hello from Person B!");
     }
 }
